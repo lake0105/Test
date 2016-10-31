ea687e5aa0e3b36cd189837f0fb133731671931f
stu <stu@qq.com>
2016-10-31 17:45:42 +0800

    1

---
+++ WebApplication1/WebApplication1/BlogDatebase.cs
@@ -0,0 +1,46 @@
+﻿namespace WebApplication1
+{
+    using System;
+    using System.Data.Entity;
+    using System.Linq;
+
+    public class BlogDatebase : DbContext
+    {
+        //您的上下文已配置为从您的应用程序的配置文件(App.config 或 Web.config)
+        //使用“Model1”连接字符串。默认情况下，此连接字符串针对您的 LocalDb 实例上的
+        //“WebApplication1.Model1”数据库。
+        // 
+        //如果您想要针对其他数据库和/或数据库提供程序，请在应用程序配置文件中修改“Model1”
+        //连接字符串。
+        public BlogDatebase()
+            : base("name=Model1")
+        {
+        }
+
+        public virtual DbSet<Blog> Blogs { get; set; }
+
+        public virtual DbSet<BlogArticle> BlogArticles { get; set; }
+
+
+
+
+
+        public class Blog
+        {
+            public int Id { get; set; }
+
+            public string Title { get; set; }
+        }
+
+        public class BlogArticle
+        {
+            public int Id { get; set; }
+
+            public int BlogId { get; set; }
+
+            public string Subject { get; set; }
+        }
+
+
+    }
+}
---
+++ WebApplication1/WebApplication1/Controllers/BlogController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using static WebApplication1.BlogDatebase;
+
+namespace WebApplication1.Controllers
+{
+    public class BlogController : Controller
+    {
+        // GET: Blog
+        public ActionResult Index()
+        {
+            var db = new BlogDatebase();
+            db.Database.CreateIfNotExists();
+            var lst = db.BlogArticles.OrderBy(o => o.Id).ToList();
+            ViewBag.BlogArticles = lst;
+            return View();
+        }
+
+        public ActionResult AddArticle()
+        {
+            return View();
+        }
+
+        public ActionResult ArticleSave(string subject,string body)
+        {
+            var article = new BlogArticle();
+            return View();
+        }
+    }
+}
