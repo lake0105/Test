6f84bbed4528e149417871e3e9a87dbbbda24742
lt <26807105@qq.com>
2016-11-08 18:46:38 +0800

    删除多余文件

---
+++ News/Controllers/BlogController.cs
@@ -0,0 +1,28 @@
+﻿using System.Linq;
+using System.Web.Mvc;
+namespace News.Controllers
+{
+    public class BlogController : Controller
+    {
+        // GET: Blog
+        public ActionResult Index()
+        {
+            var db = new BlogDatabase();
+            db.Database.CreateIfNotExists();
+            var lst = db.BlogArticles.OrderBy(o => o.Id).ToList();
+            ViewBag.BlogArticles = lst;
+            return View();
+        }
+
+        public ActionResult AddArticle()
+        {
+            return View();
+        }
+
+        public ActionResult ArticleSave(string subject,string body)
+        {
+            var article = new BlogArticle();
+            return View();
+        }
+    }
+}
